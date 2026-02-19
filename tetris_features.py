a9cba2d6129b83cc52278f53bd4f8bec4625d5ca
ozikan5 <ozankan32@gmail.com>
2026-02-18 18:19:15 -0600

    completed features for now

--- tetris_features.py
+++ tetris_features.py
@@ -35,4 +35,60 @@ def get_holes(board):
 
     return total_holes
 
+def get_bumpiness(board):
+    bumpiness = 0
+
+    # get the heights array
+    heights = get_column_heights(board)
+
+    for height in range(board.shape[1] - 1):
+        # add to the bumpiness
+
+        bumpiness += abs(heights[height] - heights[height + 1])
+
+    return bumpiness
+
+# Returns a numpy array of features: [agg_height, holes, bumpiness, max_height]
+def get_features(board):
+    col_heights = get_column_heights(board)
+    
+    # get the aggregate height
+    agg_height = sum(col_heights)
+
+    # get holes 
+    holes = get_holes(board)
+
+    # get bumpiness
+    bumpiness = get_bumpiness(board)
+
+    # get max_height
+    max_height = max(col_heights)
+
+    return np.array([agg_height, holes, bumpiness, max_height])
+
+if __name__ == "__main__":
+    
+    test_board = np.zeros((20, 10), dtype=int)
+    
+    
+    test_board[19, 0] = 1
+    test_board[18, 0] = 1
+    
+    
+    test_board[19, 1] = 1
+    test_board[17, 1] = 1 
+    
+    features = get_features(test_board)
+    print(f"Features [AggHeight, Holes, Bumpiness, MaxHeight]:")
+    print(features)
+    
+    assert features[0] == 5
+    assert features[1] == 1
+    assert features[2] == 4
+    assert features[3] == 3
+
+
+
+        
+
     
